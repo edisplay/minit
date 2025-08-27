b4fa465ef5090c1c08efad8169476ee4d0247a97
Kaspars Dambis <hi@kaspars.net>
2025-08-27 14:09:38 +0300

    Native way to do it now
    No longer PHP 5.6 support is needed

--- minit.php
+++ minit.php
@@ -10,12 +10,12 @@ Author URI: https://kaspars.net
 */
 
 // Until we add proper autoloading.
-include dirname( __FILE__ ) . '/src/minit-assets.php';
-include dirname( __FILE__ ) . '/src/minit-asset-cache.php';
-include dirname( __FILE__ ) . '/src/minit-js.php';
-include dirname( __FILE__ ) . '/src/minit-css.php';
-include dirname( __FILE__ ) . '/src/minit-plugin.php';
-include dirname( __FILE__ ) . '/src/admin.php';
-include dirname( __FILE__ ) . '/src/helpers.php';
+include __DIR__ . '/src/minit-assets.php';
+include __DIR__ . '/src/minit-asset-cache.php';
+include __DIR__ . '/src/minit-js.php';
+include __DIR__ . '/src/minit-css.php';
+include __DIR__ . '/src/minit-plugin.php';
+include __DIR__ . '/src/admin.php';
+include __DIR__ . '/src/helpers.php';
 
 add_action( 'plugins_loaded', array( 'Minit_Plugin', 'instance' ) );
